38f22dffe5e6a6f9a25c63a754adfd94d464aba5
Sheldon Hearn <sheldonh@starjuice.net>
2012-08-14 08:39:26 +0200

    Remove unused AccessControl#subject_is_sender?
    (discovered through coverage)

--- lib/interface_enforcement/access_control.rb
+++ lib/interface_enforcement/access_control.rb
@@ -9,7 +9,7 @@ module InterfaceEnforcement
 
     def allows?(sender, method)
       @sender, @method = sender, method
-      method_exists? and privacy_honoured? and protection_honoured?
+      method_exists? and !private_method? and protection_honoured?
     end
 
     private
@@ -18,16 +18,8 @@ module InterfaceEnforcement
       @subject.methods.include? @method_to_invoke
     end
 
-    def privacy_honoured?
-      !private_method? or subject_is_sender?
-    end
-
     def private_method?
-      @subject.private_methods.include? @method_to_invoke
-    end
-
-    def subject_is_sender?
-      @subject.object_id == @sender.object_id
+      @subject.respond_to?(@method_to_invoke, :private) and !@subject.respond_to?(@method_to_invoke)
     end
 
     def protection_honoured?
